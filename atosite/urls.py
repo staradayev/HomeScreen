2861fec34ae59326529310e2bf47249ccd05d34c
staradayev <staradayev@waverleysoftware.com>
2014-12-19 22:55:35 +0530

    Uniq email

--- atosite/urls.py
+++ atosite/urls.py
@@ -2,10 +2,15 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
 import autocomplete_light
+from registration.backends.default.views import RegistrationView
+from registration.forms import RegistrationFormUniqueEmail
 
 autocomplete_light.autodiscover()
 
 from care.views import UploadPictureView
+
+class RegistrationViewUniqueEmail(RegistrationView):
+    form_class = RegistrationFormUniqueEmail
 
 urlpatterns = patterns('',
     # Examples:
@@ -15,10 +20,13 @@ urlpatterns = patterns('',
     url(r'^', include('ato.urls', namespace="ato")),
     url(r'^api/', include('api.urls', namespace="api")),
     url(r'^admin/', include(admin.site.urls)),
+    
+    url(r'^accounts/register/$', RegistrationViewUniqueEmail.as_view(), name='registration_register'),
     (r'^accounts/', include('registration.backends.default.urls')),
 	
 	(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 	url(r'^autocomplete/', include('autocomplete_light.urls')),
     url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^reset/', include('password_reset.urls')),
+    
 )
