906f35ffbf2cc9b6de0e5a598962ee61f59a853b
staradayev <staradayev@waverleysoftware.com>
2015-02-08 01:42:56 +0200

    LinkTypes in admin panel readble too

--- care/admin.py
+++ care/admin.py
@@ -55,6 +55,17 @@ class LinkAdmin(admin.ModelAdmin):
 	get_name.admin_order_field = 'translations'
 
 admin.site.register(Link, LinkAdmin)
+
+class LinkTypeAdmin(admin.ModelAdmin):
+	list_display = ('get_name', 'date_pub')
+
+	def get_name(self, obj):
+		return obj.type_tag
+	get_name.short_description = 'Name'
+	get_name.admin_order_field = 'translations'
+
+admin.site.register(LinkType, LinkTypeAdmin)
+
 admin.site.register(UserProfile)
 
 class OrganizationTranslationInline(TranslationStackedInline):
@@ -70,7 +81,6 @@ class OrganizationAdmin(admin.ModelAdmin):
 	get_name.admin_order_field = 'translations'
 
 admin.site.register(Organization, OrganizationAdmin)
-admin.site.register(LinkType)
 admin.site.register(Like)
 admin.site.register(Download)
 
